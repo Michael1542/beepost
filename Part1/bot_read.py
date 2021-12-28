00ab2c9b5825e28a5ea68542a542fc1fde45a791
Michael Wang <michael.yj.wang@gmail.com>
2021-12-28 14:09:19 -0500

    test

--- Part1/bot_read.py
+++ Part1/bot_read.py
@@ -1,16 +1,21 @@
 #App to draw top landscape picture per week and then add to an image library
 import praw
 import datetime
+import requests
 
-reddit = praw.Reddit('bot1')
+class Reader:
+    reddit = praw.Reddit('bot1')
+    subreddit = reddit.subreddit("imaginarylandscapes")
+    images = []
 
-subreddit = reddit.subreddit("imaginarylandscapes")
-
-images = []
-
-for submission in subreddit.top(limit=5):
-    print("Title: ", submission.title)
-    print("Score: ", submission.score)
-    print("URL: ", submission.url)
-    print("Date: ", datetime.datetime.fromtimestamp(submission.created_utc))
-    print("---------------------------------\n")
+    def write_image(self):
+        for submission in self.subreddit.hot(limit=1):
+            print("Title: ", submission.title)
+            print("Score: ", submission.score)
+            print("URL: ", submission.url)
+            print("Date: ", datetime.datetime.fromtimestamp(submission.created_utc))
+            print("---------------------------------\n")
+            img_data = requests.get(submission.url)
+            file = open("sample_image.png", "wb")
+            file.write(img_data.content)
+            file.close()
